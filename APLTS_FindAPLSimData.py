6b17109ea81d5f9f46956983990f5d21a9b084a8
TheresaBruemmer <theresa.bruemmer@desy.de>
2020-12-10 14:40:26 +0100

    Add files via upload

---
+++ APLTS_FindAPLSimData.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append("/beegfs/desy/group/fla/ICS/tools/ModulesAndClasses/")
+import APLTS.ActivePlasmaLens as APL
+import numpy as np
+
+'''
+Input on calling this function
+'''
+gammae=float(sys.argv[1])
+sigmar_f=float(sys.argv[2])
+
+'''
+Output:
+I_0
+z_F
+'''
+
+
+'''
+Some hard coded APL data
+'''
+L_APL=0.1
+z_0=5.0e-2
+r_APL=2.0e-3
+eps_n=1.0e-6
+sigmar_i=1.0e-6
+I0_min=100.0
+I0_max=1500.0
+
+
+'''
+We want to do six runs to find the optimum.
+Each time we close in more on the opt values
+'''
+
+for opt_run in range(0,7):
+    I0_arr = np.linspace(I0_min,I0_max,10)
+    delta_I0=I0_arr[1]-I0_arr[0]
+    instances=APL.APL_setup(L=L_APL,I_0=I0_arr,z_0=z_0,r_0=r_APL,gammae=gammae,eps_n=eps_n,sigmar_i=sigmar_i)
+    match_arg = np.nanargmin(abs(instances.focalWaist()-sigmar_f))
+    I0=I0_arr[match_arg]
+    zF=instances.focalPlane()[match_arg]
+    check_Waist=instances.focalWaist()[match_arg]
+    I0_min=I0-2*delta_I0
+    I0_max=I0+2*delta_I0
+print("---------------------------")
+print("---Simulation Parameters---")
+print("I0 = " + str("%.2f" %I0)+" A")
+print("zF = " + str("%.3f" %zF)+" m")
+print("check sigmar_f = "+str("%.3f" %(check_Waist*1e6))+" um")
+print("---------------------------")
+
+
---
+++ APLTS_estimateEffEnSpread.py
@@ -0,0 +1,38 @@
+"""
+Calculates the effective energy spread in an APLTS setup. Based on Brümmer et al. "Compact all-optical tunable narrowband Compton hard X-ray source", to be published
+"""
+
+import sys
+sys.path.append("/beegfs/desy/group/fla/ICS/tools/ModulesAndClasses/")
+from APLTS import APLTS,Laser
+import APLTS.ActivePlasmaLens as APL
+
+
+
+'''
+Electron Bunch data
+'''
+gammae=65.0 #bunch central electron energy
+eps_n=1.0e-6 # normalised bunch emittance
+sigmar_i=1.0e-6 #initial bunch waist at plasma acceleration stage (PAS) exit
+'''
+Active Plasma Lens (APL) data
+'''
+L_APL=0.1 # APL length (m)
+z_0=5.0e-2 # distance PAS exit to APL (m)
+r_APL=2.0e-3 # APL radius (m)
+I0=500 # APL current (A)
+APL_instance=APL.APL_setup(L=L_APL,I_0=I0,z_0=z_0,r_0=r_APL,gammae=gammae,eps_n=eps_n,sigmar_i=sigmar_i) #initiate instance of APL setup 
+'''
+Laser data
+'''
+Ep=0.2 # laser pulse energy (J)
+tau=1.0e-12 # laser pulse FWHM duration (s)
+a0=0.1 # laser strength parameter
+llambda=1030.0e-9 # laser wavelength (m)
+Laser_instance=Laser.Laser(_wavelength=llambda,_Ep=Ep,_a0=a0,_tau_FWHM=tau) #initiate instance of Gaussian laser
+
+
+eff_enspread=APLTS.effective_energyspread_fixedsetup(Laser_instance,APL_instance)[0]
+
+print("Effective energy spread "+str("%.3f" %(eff_enspread*100)) + " %")
